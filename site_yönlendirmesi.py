ef654e75197d36e1510b668b3e46e9e691efd889
barisk802 <barisk802@gmail.com>
2025-10-20 01:27:59 +0300

    site_yönlendirmesi.py

--- site_yönlendirmesi.py
+++ site_yönlendirmesi.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import webbrowser
+import streamlit as st
 
+st.title("🌐 Site Yönlendirme")
+
+st.markdown("[📸 Instagram'a Git](https://www.instagram.com/bariskaya_1912/)")
+st.markdown("[🐦 Twitter'a Git](https://x.com/Bariskaya_1907)")
+st.markdown("[▶️ YouTube'a Git](https://www.youtube.com/@bariskaya1907)")
 st.title("🌐 Site Yönlendirme")
 
 if st.button("Instagram'a Git"):
@@ -11,5 +17,6 @@ if st.button("Twitter'a Git"):
 
 if st.button("YouTube'a Git"):
     webbrowser.open("https://www.youtube.com/@bariskaya1907")
+
 
 
