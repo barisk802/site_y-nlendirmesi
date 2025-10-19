c996800f7e42da73fb4033a446b01125c72f6a61
barisk802 <barisk802@gmail.com>
2025-10-20 01:27:27 +0300

    site_yönlendirmesi.py

--- site_yönlendirmesi.py
+++ site_yönlendirmesi.py
@@ -3,37 +3,13 @@ import webbrowser
 
 st.title("🌐 Site Yönlendirme")
 
+if st.button("Instagram'a Git"):
+    webbrowser.open("https://www.instagram.com/bariskaya_1912/")
 
-print("""
-Site Yönlendirme
+if st.button("Twitter'a Git"):
+    webbrowser.open("https://x.com/Bariskaya_1907")
 
-İşlemler:
-
-1-İnstagram'a Gitmek İçin
-
-2-Twitter'a Gitmek için
-
-3-Youtube'a Gitmek İçin
-
-Programdan Çıkmak için 'q'ya basınız.
-""")
-
-while True:
-    işlem=input("Lütfen yapmak istediğiniz işlemi seçiniz :")
-
-    if işlem=="q":
-        print("Tekrar Bekleriz")
-        break
-    elif işlem=="1":
-        print("İnstagrama Yönlendiriliyorsunuz...")
-        webbrowser.open("https://www.instagram.com/bariskaya_1912/")
-    elif işlem=="2":
-        print("Twitter'a Yönlendiriliyorsunuz...")
-        webbrowser.open("https://x.com/Bariskaya_1907")
-    elif işlem=="3":
-        print("Youtube'a Yönlendiriliyorsunuz...")
-        webbrowser.open("https://www.youtube.com/@bariskaya1907")
-    else:
-        print("Geçersiz İşlem.")
+if st.button("YouTube'a Git"):
+    webbrowser.open("https://www.youtube.com/@bariskaya1907")
 
 
