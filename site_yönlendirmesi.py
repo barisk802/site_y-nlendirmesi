c4158a7b94f2ecf218923968df4b9b6b96bd0d31
barisk802 <barisk802@gmail.com>
2025-10-20 01:28:24 +0300

    site_yönlendirmesi.py

--- site_yönlendirmesi.py
+++ site_yönlendirmesi.py
@@ -7,16 +7,7 @@ st.title("🌐 Site Yönlendirme")
 st.markdown("[📸 Instagram'a Git](https://www.instagram.com/bariskaya_1912/)")
 st.markdown("[🐦 Twitter'a Git](https://x.com/Bariskaya_1907)")
 st.markdown("[▶️ YouTube'a Git](https://www.youtube.com/@bariskaya1907)")
-st.title("🌐 Site Yönlendirme")
 
-if st.button("Instagram'a Git"):
-    webbrowser.open("https://www.instagram.com/bariskaya_1912/")
-
-if st.button("Twitter'a Git"):
-    webbrowser.open("https://x.com/Bariskaya_1907")
-
-if st.button("YouTube'a Git"):
-    webbrowser.open("https://www.youtube.com/@bariskaya1907")
 
 
 
